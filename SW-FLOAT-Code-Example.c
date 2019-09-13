c661d2ec2a0d48376ab58652d84f594ec926b20b
Johan Conradie <4562957+microrobotics@users.noreply.github.com>
2019-09-13 08:12:22 +0200

    Add files via upload

---
+++ SW-FLOAT-Code-Example.c
@@ -0,0 +1,20 @@
+int ledPin = 10;         // LED Indicator
+int level_sensor = 7;    // Level Switch
+int status = 0;          // variable for reading the pin status
+
+void setup() {
+  pinMode(ledPin, OUTPUT);         // declare LED pin as output
+  pinMode(level_sensor, INPUT);    // declare levelSensor pin as input
+}
+
+void loop()
+{
+  status = digitalRead(level_sensor);      // read input value
+  if (status == LOW)                       // check if the input 
+    {    
+    digitalWrite(ledPin, HIGH);         // Turn LED ON 
+    } else                                    
+    {
+    digitalWrite(ledPin, LOW);          // Turn LED OFF
+  }
+}
